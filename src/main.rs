8267e84d21854763686ebfa86a499223d859eb6d
Chaitanya Kadu <chaitanyakadu03@gmail.com>
2025-02-25 19:28:57 +0530

    Simple tcp server
    This is much cleaner tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,62 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{BufReader, prelude::*};
-use std::fs;
+use std::{
+    fs,
+    net::{ TcpListener, TcpStream },
+    io::{ BufReader, prelude::* }
+};
 
 fn main() {
 
-    let listner = TcpListener::bind("localhost:7878").unwrap();
+    match TcpListener::bind("127.0.0.1:7878") {
 
-    for stream in listner.incoming() {
-        
-        let stream = stream.unwrap();
+        Ok(listener) => {
+            
+            for stream in listener.incoming() {
 
-        handle_connection(stream);   
+                request_handler(stream);
+
+            };
+
+        },
+
+        Err(err) => println!("{:#?}", err)
+
     }
-    
+
 }
 
-fn handle_connection(mut stream: TcpStream){
-    
-    let buf_reader = BufReader::new(&stream);
-    
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+fn request_handler(mut stream: Result<TcpStream, std::io::Error>) {
 
-    let (status_line, contents) =  if request_line == "GET / HTTP/1.1" {
-        
-        ("HTTP/1.1 200 OK", fs::read_to_string("home.html").unwrap())
+    match stream {
 
-    } else {
+        Ok(mut stream) => {
 
-        ("HTTP/1.1 404 NOT FOUND", fs::read_to_string("404.html").unwrap())
+            let buf_reader = BufReader::new(&stream);
 
-    };
+            let request_line = buf_reader
+                .lines()
+                .next()
+                .unwrap()
+                .unwrap();
 
-    let length = contents.len();
+            let (status_line, text) = if request_line == "GET / HTTP/1.1" {
+                ("HTTP:/1.1 200 OK", "<h1>Home Page</h1>")
+            } else if request_line == "GET /about HTTP/1.1" {
+                ("HTTP:/1.1 200 OK", "<h1>About Page</h1>")
+            } else {
+                ("HTTP:/1.1 404 PAGE NOT FOUND", "<h1>Page not found!</h1>")
+            };
 
-    let response = format!(
-            "{status_line}\r\nContent-Length : {length}\r\n\r\n{contents}"
-        );
+            let content = String::from(text);
+            let length = content.len();
 
-    stream.write_all(response.as_bytes()).unwrap();
+            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+            stream.write_all(response.as_bytes()).unwrap();
+
+        },
+
+        Err(err) => println!("{:#?}", err)
+
+    }
 
 }
